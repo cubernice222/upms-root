635a619e253013d9b333d165b14092444db7f424
cuber <cuber.carrots@gmail.com>
2018-05-10 21:03:14 +0800

    加上codegen.output 及别的

--- umps-rmi/src/main/java/org/cuber/umps/rmi/UserBizRmiService.java
+++ umps-rmi/src/main/java/org/cuber/umps/rmi/UserBizRmiService.java
@@ -2,14 +2,15 @@ package org.cuber.umps.rmi;
 
 import io.vertx.codegen.annotations.Fluent;
 import io.vertx.codegen.annotations.ProxyGen;
+import io.vertx.codegen.annotations.VertxGen;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import org.cuber.umps.bo.SeekUserReq;
 import org.cuber.umps.bo.SeekUserResp;
 
+@VertxGen
 @ProxyGen
-public interface UserBizRmi {
-
+public interface UserBizRmiService {
     @Fluent
-    UserBizRmi seekUser(SeekUserReq req, Handler<AsyncResult<SeekUserResp>> seekHandler);
+    UserBizRmiService seekUser(SeekUserReq req, Handler<AsyncResult<SeekUserResp>> seekHandler);
 }
